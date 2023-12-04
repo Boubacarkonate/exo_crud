2ce50346e76285fb272f0e27c5c58c9f2f133ae7
Bouba <boubacar.konate@outlook.fr>
2023-12-04 14:36:23 +0100

    READ ok

--- crud.php
+++ crud.php
@@ -1,9 +1,14 @@
 <?php
 
-var_dump($_POST);
+// var_dump($_POST);
 
 //connexion à la base de données avec new mysqli
-
+/*
+new mysqli est une approche orientée objet pour établir une connexion MySQLi.
+Elle crée un nouvel objet de la classe mysqli.
+Elle prend généralement quatre paramètres : l'hôte (localhost), le nom d'utilisateur, le mot de passe et le nom de la base de données.
+Elle peut également prendre deux paramètres supplémentaires pour spécifier le port et le socket, mais ils sont facultatifs.
+ */
 $connect = new mysqli('localhost', 'root', '', 'cda_contact');
 
 if (isset($_POST['save'])) {
@@ -19,6 +24,11 @@ if (isset($_POST['save'])) {
         $connect->query("INSERT INTO crud (name, city, photo) VALUES ('$name', '$city', '".$photo."')") or die($connect->error);
        
         move_uploaded_file($_FILES['photo']['tmp_name'], 'upload/'.$photo);
-        echo "succes";
+        // echo "succes";
+
+        //je définis un clef 'message' à mon $_SESSION avec sa valeur "Données ajoutées
+        $_SESSION['message'] = "Données ajoutées";
+        $_SESSION['msg_type'] = "success";
+        header("location:index.php");
     }
 }
--- index.php
+++ index.php
@@ -7,47 +7,77 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        h1,
-        h2 {
-            text-align: center;
-        }
-
-        h2 {
-            font-size: large;
-        }
-        h2 .color_letter {
-            color: red;
-            font-size: 130%;
-}
+        h1,h2 {text-align: center;}
+        h2 {font-size: large;}
+        h2 .color_letter {color: red;font-size: 130%; }
     </style>
 </head>
 
 <body>
+    <?php require_once "crud.php" ?>
+
+    <?php if (isset($_SESSION['message'])) : ?>
+        <div class="alert alert-<? $_SESSION['msg_type']; ?>" role="alert"> <!--alert boostrap. A la place d'écrire la couleur de l'alerte, je la remplace par la $_SESSION[msg_type] déclaré dans crud.php-->
+            <?php
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+            ?>
+        </div>
+    <?php endif; ?>
+
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <h1>CRUD</h1>
                 <h2><span class="color_letter">C</span>reate <span class="color_letter">R</span>ead <span class="color_letter">U</span>pdate <span class="color_letter">D</span>elete</h2>
             </div>
-            <div class="row">
-                <form action="crud.php" method="post" enctype="multipart/form-data">
-                    <div class="mb-3">
-                        <label for="nom" class="form-label">Nom</label>
-                        <input type="text" name="nom" class="form-control" id="" placeholder="Votre nom">
-                    </div>
-                    <div class="mb-3">
-                        <label for="ville" class="form-label">Ville</label>
-                        <input type="text" name="ville" class="form-control" id="" placeholder="Votre ville">
-                    </div>
-                    <div class="mb-3">
-                        <label for="photo" class="form-label">Photo :</label>
-                        <input type="file" name="photo" class="form-control" id="">
-                    </div>
-                    <button type="submit" name="save" class="btn btn-primary">CREATE</button>
-
-                </form>
-            </div>
+        </div> <!----->
+        <div class="row">
+            <?php
+            $mysqli = new mysqli('localhost', 'root', '', 'cda_contact') or die(mysqli_error($mysqli));     //connexion à la BD
+            $result = $mysqli->query("SELECT * FROM crud") or die($mysqli->error);                          //requête + gestion des erreurs
+            // print_r($result);
+            ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>NOM</th>
+                        <th>VILLE</th>
+                        <th>PHOTO</th>
+                        <th>ACTION</th>
+                    </tr>
+                </thead>
+                <?php while($row = $result->fetch_assoc()) :?>
+                    <tr>
+                        <td><?php echo $row['name'] ?></td>
+                        <td><?php echo $row['city'] ?></td>
+                        <td><img src="upload/<?php echo $row['photo']; ?>" alt="" class="img-fluid" style="width: :20%;"></td>
+                        <td>
+                            <a href="crud.php?delete=<?php echo $row['id'] ?>">SUPPRIMER</a>
+                        </td>
+                    </tr>
+                    <?php endwhile ?>
+            </table>
         </div>
+        <div class="row">
+            <form action="crud.php" method="post" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label for="nom" class="form-label">Nom</label>
+                    <input type="text" name="nom" class="form-control" id="" placeholder="Votre nom">
+                </div>
+                <div class="mb-3">
+                    <label for="ville" class="form-label">Ville</label>
+                    <input type="text" name="ville" class="form-control" id="" placeholder="Votre ville">
+                </div>
+                <div class="mb-3">
+                    <label for="photo" class="form-label">Photo :</label>
+                    <input type="file" name="photo" class="form-control" id="">
+                </div>
+                <button type="submit" name="save" class="btn btn-primary">CREATE</button>
+
+            </form>
+        </div>
+    </div>
     </div>
 
 
