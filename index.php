23f2d5b5d7e830906d634fe7097d485e00d9dc8f
Bouba <boubacar.konate@outlook.fr>
2024-01-03 12:44:38 +0100

    modification nom du fichier Jenkinsfile

--- index.php
+++ index.php
@@ -100,3 +100,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
+<!-- pipeline{
+    agent any
+    stages{
+        stage('verify version') {
+           steps {
+            sh 'php --version'
+           } 
+        }
+        stage('run file') {
+            steps {
+                sh 'php index.php'
+            }
+        }
+    }
+} -->
