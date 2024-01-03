f70f723ee9fc7ed5c5603f76e900bfa1b404fad4
Bouba <boubacar.konate@outlook.fr>
2024-01-03 12:55:59 +0100

    test jenkinsfile avec fichier hello.php

--- index.php
+++ index.php
@@ -102,18 +102,3 @@
 </html>
 
 
-<!-- pipeline{
-    agent any
-    stages{
-        stage('verify version') {
-           steps {
-            sh 'php --version'
-           } 
-        }
-        stage('run file') {
-            steps {
-                sh 'php index.php'
-            }
-        }
-    }
-} -->
